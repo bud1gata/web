901de6e423481fd315ee22b29480fce02c879cd6
bud1gata <bud1gata@gmail.com>
2024-05-27 14:07:34 +0700

    add refresh function, edit modal

--- js/javascript.js
+++ js/javascript.js
@@ -10,22 +10,39 @@ document.getElementById('studentForm').addEventListener('submit', function(event
   }
 
   // Check if the student number is in the array
-  const studentNumbers = ['123', '456', '789']; // Input array
+  const studentNumbers = ['1003','1004','1008','1009','1011','1013','1020','1022','1027','1029','1033','1036','1039','1040','1044','1046','2002','2005','2007','2009','2010','2011','2016','2018','2020','2025','2026','2030','2035','2038']; // Input array
   if (studentNumbers.includes(studentNumber)) {
     // Display success message in modal
-    document.getElementById('resultText').textContent = `Alhamdulillah, Nomor Induk Siswa (NIS): "${studentNumber}" telah dapat mengikuti ujian.`;
+    document.getElementById('resultText').textContent = `Alhamdulillah, Nomor Kode Ujian : "${studentNumber}" telah dapat mengikuti ujian.`;
     document.getElementById('modalBody').classList.add('bg-info');
+    document.getElementById('btnStatus').classList.add('btn-success');
+    document.getElementById('btnStatus').textContent = `Ayo ke Halaman Ujian`;
   } else {
     // Display error message in modal
-    document.getElementById('resultText').textContent = `Mohon maaf Nomor Induk Siswa (NIS): "${studentNumber}" belum dapat mengikuti ujian.`;
+    document.getElementById('resultText').textContent = `Mohon maaf Nomor Kode Ujian: "${studentNumber}" belum dapat mengikuti ujian.`;
     document.getElementById('modalBody').classList.add('bg-danger');
+    document.getElementById('btnStatus').classList.add('btn-secondary');
+    document.getElementById('btnStatus').textContent = `Halaman Ujian`;    
+    document.getElementById('linkUjian').href = "#";
+    document.getElementById('btnStatus').disabled=true;
   }
 
   // Show the modal
   $('#resultModal').modal('show');
 });
+
   // Add event listener to the button
   document.getElementById('refreshButton').addEventListener('click', function() {
     // Reload the page
     location.reload();
   });
+
+  document.getElementById('closeModalButton').addEventListener('click', function() {
+    location.reload();
+});
+// Add event listener to the modal to detect when it's about to be hidden
+var myModalEl = document.getElementById('resultModal');
+myModalEl.addEventListener('hide.bs.modal', function () {
+    // Reload the page when the modal is about to be hidden (closed)
+    location.reload();
+});
